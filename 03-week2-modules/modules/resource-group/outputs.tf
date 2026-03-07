c40d29e9bba82542d1aef76971795e0826ad17f7
KosnikM <mkosnikk@gmail.com>
2026-03-07 22:45:17 +0100

    implement resource-group child module

--- 03-week2-modules/modules/resource-group/outputs.tf
+++ 03-week2-modules/modules/resource-group/outputs.tf
@@ -1,1 +1,10 @@
 # Outputs
+output "name" {
+  description = "resource group name"
+  value = azurerm_resource_group.this.name
+}
+
+output "id" {
+  description = "resource group ID"
+  value = azurerm_resource_group.this.id
+}
--- 03-week2-modules/modules/resource-group/variables.tf
+++ 03-week2-modules/modules/resource-group/variables.tf
@@ -1,1 +1,17 @@
 # Variables
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+  default = "polandcentral"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    
+  }
+
+}
