4781c94e19fc5cacc9700f98bd2ecfb71985756e
KosnikM <mkosnikk@gmail.com>
2026-03-11 01:04:39 +0100

    feat: add networking outputs to root module

--- 03-week2-modules/outputs.tf
+++ 03-week2-modules/outputs.tf
@@ -18,3 +18,25 @@ output "staging_id" {
   description = "resource group id"
   value       = module.rg_staging.id
 }
+
+output "vnet_name" {
+    value       = module.networking.vnet_name
+}
+output "vnet_id" {
+    value       = module.networking.vnet_id
+}
+output "subnet_name" {
+    value       = module.networking.subnet_name
+}
+output "subnet_id" {
+    value       = module.networking.subnet_id
+}
+output "nsg_name" {
+    value       = module.networking.nsg_name
+}
+output "nsg_id" {
+    value       = module.networking.nsg_id
+}
+
+
+
